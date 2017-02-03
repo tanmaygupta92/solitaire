e13da0514d283333e2c8d7b69090e1a737472400
tanmaygupta92 <tanmayg@cybage.com>
2017-02-03 11:52:28 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
 
     frameworks: ["jasmine"],
 
-    browsers: ["Chrome", "Firefox"],
+    browsers: ["Chrome", "Firefox","PhantomJS"],
 
     plugins: [
       "karma-chrome-launcher",
